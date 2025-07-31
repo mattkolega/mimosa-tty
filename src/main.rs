9cfa6766bd84294ba011a8a998a52efa86ac19a1
Matthew Kolega <12107095+mattkolega@users.noreply.github.com>
2025-07-31 19:15:03 +1000

    Get input working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,52 @@
+use std::{io::{self, Write}};
+
 fn main() {
-    println!("Hello, world!");
+    let pkg_version = env!("CARGO_PKG_VERSION");
+    println!("mimosa-tty ({})", pkg_version);
+
+    loop {
+        print!("> ");
+        io::stdout().flush().expect("Failed to flush output.");
+
+        let mut input = String::new();
+
+        io::stdin().read_line(&mut input).expect("Failed to get input.");
+
+        let cleaned_input = input.trim();
+        if cleaned_input.is_empty() { continue; }
+
+        match handle_command(&cleaned_input) {
+            Ok(result) => match result {
+                CommandResult::Continue => (),
+                CommandResult::Exit => break,
+            },
+            Err(e) => {
+                eprintln!("{}", e);
+                break;
+            }
+        }
+    }
+}
+
+enum CommandResult {
+    Continue,
+    Exit
+}
+
+/// Parses input and runs a command
+fn handle_command(user_input: &str) -> Result<CommandResult, String> {
+    let mut parts = user_input.split_whitespace();
+    let command = parts.next().unwrap();
+
+    let mut result = CommandResult::Continue;
+
+    match command {
+        "exit" => {
+            println!("Exiting the program...");
+            result = CommandResult::Exit;
+        },
+        _ => println!("Unknown command")
+    };
+
+    Ok(result)
 }
